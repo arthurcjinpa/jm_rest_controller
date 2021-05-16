ec39e0d2217fe2c45d71cd72885fdbed66818d9a
arthurcjinpa <73398493+arthurcjinpa@users.noreply.github.com>
2021-05-17 01:58:36 +0200

    Lombok removed

--- [3.1.3] spring_rest_controller/src/main/java/artur/model/Role.java
+++ [3.1.3] spring_rest_controller/src/main/java/artur/model/Role.java
@@ -1,6 +1,5 @@
 package artur.model;
 
-import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
@@ -10,10 +9,6 @@ import javax.persistence.Table;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@EqualsAndHashCode
 @Entity
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
@@ -24,6 +19,32 @@ public class Role implements GrantedAuthority {
 
     @Column(unique = true)
     private String name;
+
+    public Role() {
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != null ? !id.equals(role.id) : role.id != null) return false;
+        return name != null ? name.equals(role.name) : role.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 
     public Long getId() {
         return id;
@@ -44,5 +65,13 @@ public class Role implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
